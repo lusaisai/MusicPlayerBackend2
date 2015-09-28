e31389a05eb2a49436ef75b834f543eb32cb1f66
lusaisai <lusaisai@163.com>
2015-09-29 06:00:16 +0800

    TDD

--- MusicPlayerBackend2/local_settings.py
+++ MusicPlayerBackend2/local_settings.py
@@ -1,7 +1,9 @@
 from settings import *
+
+# music from localhost
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.test.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, 'db.local.sqlite3'),
         }
     }
